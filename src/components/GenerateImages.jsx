75d049f866357da9260c200499185cebd6507c4f
CodesIAGenerator <baudirguanche@gmail.com>
2023-06-12 10:34:55 +0100

    Se ha mejorado el diseño

--- src/components/GenerateImages.jsx
+++ src/components/GenerateImages.jsx
@@ -5,6 +5,7 @@ import { useNavigate } from 'react-router-dom';
 import './../GenerateImages.css';
 
 
+
 const GenerateImages = () => {
 	const [user, setUser] = useState(null);
 	const navigate = useNavigate();
@@ -23,27 +24,25 @@ const GenerateImages = () => {
 
 	
 
-  return (
-	  <div className='test home-container min-vh-100 pt-5'>
-		  <div className='row'>
-			  <div className="mx-3 col-md-5 card-1">
-			  <h2 className='text-center'>Generador de imagenes</h2>
-			  <p className='text-center'>Desata tu creatividad y descubre un mundo de posibilidades con nuestra plataforma de generación de imágenes con IA. Crea obras únicas y sorprendentes en tan solo unos clics.</p>
+	return (
+		<div className='home-container min-vh-100 pt-5'>
+		  <div className='container'>
+			<div className='row justify-content-md-between overflow-auto'>
+			  <div className="mx-3 col-md-5 card-1 p-4 bg-dark text-light rounded">
+				<h2 className='text-center mb-3'>Generador de imagenes</h2>
+				<p className='text-center'>Desata tu creatividad y descubre un mundo de posibilidades con nuestra plataforma de generación de imágenes con IA. Crea obras únicas y sorprendentes en tan solo unos clics.</p>
 			  </div>
-
+	  
 			  <div className="col-md-5">
-				  <div className="d-flex">
-				  	<input id='promptImage' type="text" className='form-control bg-dark text-white' placeholder='Introduce el prompt' />
-				  	<button className='btn btn-success'>Generar</button>
-				  </div>
-				  
+				<div className="d-flex align-items-center justify-content-center mt-3">
+				  <input id='promptImage' type="text" className='form-control bg-dark text-white mr-2' placeholder='Introduce el prompt' />
+				  <button className='btn btn-success'>Generar</button>
+				</div>
 			  </div>
-			  
+			</div>
 		  </div>
-		  
-		  
-	  </div>
-  )
+		</div>
+	  )	  
 }
 
 export default GenerateImages
